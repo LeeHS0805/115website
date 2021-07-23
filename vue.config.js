10c37522a538e097c044c32edd28b9fbc98c6c39
LeeHS0805 <504794559@qq.com>
2021-07-23 18:19:17 +0800

    cdn change

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,37 @@
-module.exports = {
+let cdn = {
+    css: [
+
+    ],
+    js: [
+        // vue must at first!
+        'https://cdn.jsdelivr.net/npm/vue@2.6/dist/vue.min.js', // vuejs
+        // vue-router
+        'https://unpkg.com/vue-router/dist/vue-router.js',
+        // axios
+        'https://cdn.bootcdn.net/ajax/libs/axios/0.21.1/axios.min.js',
+        // Vuex
+        'https://cdn.bootcss.com/vuex/3.0.1/vuex.min.js'
+
+    ]
+}
+let externals={
+    'vue': 'Vue',
+    'vue-router': 'VueRouter',
+    'axios': 'axios',
+    'vuex': 'Vuex',
+}
+
+module.exports={
+    // publicPath:'/',
     lintOnSave: false,
-    runtimeCompiler: true
+    runtimeCompiler: true,
+    configureWebpack:{
+        externals:externals
+    },
+    chainWebpack (config) {
+        config.plugin('html').tap(args => {
+            args[0].cdn = cdn
+            return args
+        })
+    }
 }
